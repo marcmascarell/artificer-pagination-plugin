8943986bcbc12a3711784eac36c3474a5bcaa14f
Marc Mascarell <marcmascarell@gmail.com>
2015-10-11 01:19:34 +0200

    [WIP] + added extension manager tests = improved plugins, now are easiest to register and more flexible = Refactored Extension Manager

--- src/ArtificerPaginationPluginServiceProvider.php
+++ src/ArtificerPaginationPluginServiceProvider.php
@@ -1,6 +1,7 @@
 <?php namespace Mascame\ArtificerPaginationPlugin;
 
 use Illuminate\Support\ServiceProvider;
+use Mascame\Artificer\Artificer;
 use Mascame\Artificer\Plugin\PluginManager;
 use Mascame\ArtificerPaginationPlugin;
 use Route;
@@ -8,7 +9,8 @@ use App;
 
 class ArtificerPaginationPluginServiceProvider extends ServiceProvider {
 
-    protected $name = 'mascame/artificer-pagination-plugin';
+    protected $name = 'artificer-pagination-plugin';
+
 	/**
 	 * Indicates if loading of the provider is deferred.
 	 *
@@ -33,21 +35,23 @@ class ArtificerPaginationPluginServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
-        $name = $this->name;
+        if (Artificer::isBooted()) {
+            $name = $this->name;
 
-        App::bind($this->name, function () use ($name) {
-            return new PaginationPlugin($name);
-        });
-
-        $pluginManager = new PluginManager($this->name);
-
-        $pluginManager->addRoutes(function () {
-            Route::group(array('prefix' => 'model'), function () {
-                Route::get('{slug}', array('as' => 'admin.model.all', 'uses' => '\Mascame\ArtificerPaginationPlugin\PaginationController@all'));
-                Route::post('{slug}/filter', array('as' => 'admin.model.filter', 'uses' => '\Mascame\ArtificerPaginationPlugin\PaginationController@filter'));
-                Route::post('{slug}/pagination', array('as' => 'admin.model.pagination', 'uses' => '\Mascame\ArtificerPaginationPlugin\PaginationController@paginate'));
+            App::bind($this->name, function () use ($name) {
+                return new PaginationPlugin($name);
             });
-        });
+
+            $pluginManager = new PluginManager($this->name);
+
+            $pluginManager->addRoutes(function () {
+                Route::group(array('prefix' => 'model'), function () {
+                    Route::get('{slug}', array('as' => 'admin.model.all', 'uses' => '\Mascame\ArtificerPaginationPlugin\PaginationController@all'));
+                    Route::post('{slug}/filter', array('as' => 'admin.model.filter', 'uses' => '\Mascame\ArtificerPaginationPlugin\PaginationController@filter'));
+                    Route::post('{slug}/pagination', array('as' => 'admin.model.pagination', 'uses' => '\Mascame\ArtificerPaginationPlugin\PaginationController@paginate'));
+                });
+            });
+        }
 	}
 
     private function addPublishCommand()
